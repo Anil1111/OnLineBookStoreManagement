0f1d26e1b40439fac613879c13e0f29873b4fa46
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-09-03 15:56:19 +0530

    OnLine

--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -5,10 +5,41 @@ using System.Text;
 
 namespace ConsoleApp35
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
+            bool isShopping = true;
+            List<Book> myBook = new List<Book>();
+            while (isShopping)
+            {
+                Book b = new Book();
+                var purchaseBook = new Book();
+                b.Title = Console.ReadLine();
+                b.Price = Convert.ToDouble(Console.ReadLine());
+                myBook.Add(purchaseBook);
+
+                Console.WriteLine("Continue Shopping yes or no");
+                var choice = Console.ReadLine();
+
+                if (choice == "no")
+                    isShopping = false;
+
+
+         }
+
+            Console.WriteLine("Here Are Your Books");
+
+            foreach (var Book in myBook)
+                Console.WriteLine("Title  " + Book.Title + "  Price   " + Book.Price);
+
+
+
+
         }
     }
+
+    
+
 }
