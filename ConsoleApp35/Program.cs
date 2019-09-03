ad979452dfcf2be5a53164f90c3427df66b0e8c4
TheAnilDeveloper <anilsri.04@gmail.com>
2019-09-03 16:42:44 +0530

    Update Program.cs

--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ConsoleApp35
 {
-   
+
     class Program
     {
         static void Main(string[] args)
@@ -14,32 +14,39 @@ namespace ConsoleApp35
             List<Book> myBook = new List<Book>();
             while (isShopping)
             {
-                Book b = new Book();
-                var purchaseBook = new Book();
-                b.Title = Console.ReadLine();
-                b.Price = Convert.ToDouble(Console.ReadLine());
-                myBook.Add(purchaseBook);
+                //Book b = new Book();
+                Console.WriteLine("welcome To Online Book Shopping");
+                
+                var purchasedBook = new Book();
+                Console.WriteLine("Pl Enter Book Title");
+                purchasedBook.Title = Console.ReadLine();
+                Console.WriteLine("Pl Enter Book Price");
+                purchasedBook.Price = Convert.ToDouble(Console.ReadLine());
+                myBook.Add(purchasedBook);
 
                 Console.WriteLine("Continue Shopping yes or no");
                 var choice = Console.ReadLine();
 
                 if (choice == "no")
+                {
+                    var total = 0.0;
                     isShopping = false;
+                    Console.WriteLine("Here Are Your Books");
+                    foreach (var Book in myBook)
+                    {
+                        total += Book.Price;
+                        Console.WriteLine("Title  " + Book.Title + "  Price   " + Book.Price);
+                    }
 
+                    Console.WriteLine("        Hello Total Price is  Wishing You coming soon    " + total);
 
-         }
-
-            Console.WriteLine("Here Are Your Books");
-
-            foreach (var Book in myBook)
-                Console.WriteLine("Title  " + Book.Title + "  Price   " + Book.Price);
-
-
-
+               }
+            }
 
         }
     }
+}
 
     
 
-}
+
